c70a595bf44a3b82eb645eca31f0933b708b8a09
StevenQuintanaGT <msquintana.gt@gmail.com>
2024-02-12 23:03:30 -0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function hacerCrecerBotonNo() {
+    var btnSi = document.getElementById('btnSi');
+    btnSi.style.transform = 'scale(2.0)'; 
+
+    var btnNo = document.getElementById('btnNo');
+    btnNo.style.display = 'none';
+
+    var h3No = document.getElementById('h3No');
+    h3No.innerText="COMOOO QUE NOOOO??? PIPIPI INTENTA OTRA VEZ"
+}
+
+function redirigirAOtraPagina() {
+    // Cambia la ubicación del navegador
+    window.location.href = '/si.html';
+}
